5ec37c347b225f05117ba7b49ab68d1780bcc901
Deanna  Hartsook <deanna.hartsook14@imperial.ac.uk>
2016-02-07 13:42:35 -0800

    got collapsing cities and activities to work

--- trips.js
+++ trips.js
@@ -2,18 +2,24 @@ if (Meteor.isClient) {
   // TODO: move this to server and make it into legit database
   Template.body.helpers({
     cities: [
-      { id: "1" },
-      { id: "2" },
-      { id: "3" }
+      { city_id: "1" },
+      { city_id: "2" },
+      { city_id: "3" }
     ]
   });
 
   Template.city.helpers({
     activities: [
-      { id: "1" },
-      { id: "2" },
-      { id: "3" }
+      { activity_id: "1" },
+      { activity_id: "2" },
+      { activity_id: "3" }
     ]
+  });
+
+  Template.activity.helpers({
+    city: function() {
+      return Template.parentData(2).city
+    },
   });
 
 }
