3e5b347408b8592dc592c320841e583240da854f
Deanna  Hartsook <deanna.hartsook14@imperial.ac.uk>
2016-02-07 23:27:22 -0800

    added basic collections for cities and activities. Have to insert manaully

--- trips.js
+++ trips.js
@@ -1,19 +1,45 @@
+CitiesCollection = new Mongo.Collection("cities");
+ActivitiesCollection = new Mongo.Collection("activities");
+
+/*
+  db.cities.insert({name: "London", createdAt: new Date() });
+  City: {
+    name: unique shorter string
+    start date: Date
+    end date: Date
+    description: (long) string
+    activities: [ActivityId], client does not have access to
+    createdAt: Date, created by default
+    notes: long string
+  }
+*/
+
+/*
+  db.activities.insert({name: "London Eye", createdAt: new Date(), activity_id: 1});
+  Activity: {
+    city_id: client does not have access to
+    name: short string
+    description: long string
+    opening_times: object probably
+    price: object probably
+    notes: long string
+    createdAt: Date, created by default
+  }
+*/
+
+
 if (Meteor.isClient) {
   // TODO: move this to server and make it into legit database
   Template.body.helpers({
-    cities: [
-      { city_id: "1" },
-      { city_id: "2" },
-      { city_id: "3" }
-    ]
+    cities: function() {
+      return CitiesCollection.find({});
+    }
   });
 
   Template.city.helpers({
-    activities: [
-      { activity_id: "1" },
-      { activity_id: "2" },
-      { activity_id: "3" }
-    ]
+    activities: function() {
+      return ActivitiesCollection.find({});
+    }
   });
 
   Template.activity.helpers({
