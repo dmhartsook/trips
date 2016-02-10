3ef5992b7642922bf0f25ebc4b19933152b2f2a6
Deanna  Hartsook <deanna.hartsook14@imperial.ac.uk>
2016-02-09 23:18:45 -0800

    added UI for adding activity

--- client/client.js
+++ client/client.js
@@ -1,8 +1,31 @@
 Meteor.subscribe("cities");
 
+addTemplateEnum = {
+  UNKNOWN: 0,
+  CITY: 1,
+  ACTIVITY: 2
+};
+
+Session.setDefault('addItemFieldsTemplate', addTemplateEnum.UNKNOWN);
+
 Template.body.helpers({
   cities: function() {
     return CitiesCollection.find({});
+  }
+});
+
+Template.body.events({
+  'click .add-item-list': function(event) {
+    var addTemplate = addTemplateEnum.UNKNOWN;
+    switch(event.target.id) {
+      case "add-city-btn":
+        addTemplate = addTemplateEnum.CITY;
+        break;
+      case "add-activity-btn":
+        addTemplate = addTemplateEnum.ACTIVITY;
+        break;
+    }
+    Session.set('addItemFieldsTemplate', addTemplate);
   }
 });
 
@@ -22,6 +45,24 @@ Template.addItemModal.events({
 
 Template.addItemModal.helpers({
   addItemFieldsTemplate: function() {
-    return "insertCityFields";
+    switch(Session.get('addItemFieldsTemplate')) {
+      case addTemplateEnum.CITY:
+        return "insertCityFields";
+      case addTemplateEnum.ACTIVITY:
+        return "insertActivityFields";
+      default:
+        return "";
+    }
+    
+  }, 
+  title: function() {
+    switch(Session.get('addItemFieldsTemplate')) {
+      case addTemplateEnum.CITY:
+        return "Add a City";
+      case addTemplateEnum.ACTIVITY:
+        return "Add an Actvity";
+      default:
+        return "";
+    }
   }
-})
+});
